536db56b1d17f17c99d5120c65c948fff1ba1b65
CarnunMP <carnun@hotmail.co.uk>
2019-08-22 14:09:51 +0100

    l-c Instructor done

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -10,3 +10,20 @@ class Person {
         return `Hello my name is ${this.name}, I am from ${this.location}.`
     }
 }
+
+class Instructor extends Person {
+    constructor(name, age, location, specialty, favLanguage, catchPhrase) {
+        super(name, age, location);
+        this.specialty = specialty;
+        this.favLanguage = favLanguage;
+        this.catchPhrase=catchPhrase;
+    }
+
+    demo(subjectString) {
+        console.log(`Today we are learning about ${subjectString}.`);
+    }
+
+    grade(studentObject, subjectString) {
+        console.log(`${studentObject.name} receives a perfect score on ${subjectString}`);
+    }
+}
